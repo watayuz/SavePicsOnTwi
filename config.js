b66789b8b7371fc06b824c3467c43ec0b222fecf
watayuz <w-desu@i.softbank.jp>
2016-11-29 02:53:50 +0900

    ファイルの削除

--- config.js
+++
@@ -1,4 +0,0 @@
-exports.consumer_key: '【Consumer Keyの内容】',
-exports.consumer_secret: '【Consymer Secretの内容】',
-exports.access_token_key: '【Access Token Keyの内容】',
-exports.access_token_secret: '【Access Token Secretの内容】'
