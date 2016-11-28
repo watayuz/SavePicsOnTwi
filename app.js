b8503c5864dcf2e7b2847c41c3be73a6ac4d616d
watayuz <w-desu@i.softbank.jp>
2016-11-29 03:14:33 +0900

    作業途中

--- app.js
+++ app.js
@@ -2,11 +2,24 @@
 
 // TLの画像を保存するやつ
 
-const config = require('config.js');
-const twitter = require('twitter');
+const config = require('./config.js');
+const Twitter = require('twitter');
 const path = require('path');
 const SAVEDIR = path.join(__dirname, 'pics');
 
-twitter.stream('statuses/filter', {}, stream => {
+const client = new Twitter({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token_key: config.access_token_key,
+    access_token_secret: config.access_token_secret
+});
 
+client.stream('statuses/filter', {track: ''}, (stream) => {
+    stream.on('data', tweet => {
+        console.log('on data user: ${tweet.text}');
+    });
+
+    stream.on('error', error => {
+        console.log('on error: ${error}');
+    });
 });
