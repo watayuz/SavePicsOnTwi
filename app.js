637614e7eadd2933e9b7967e103a9c27b1dbab4b
watayuz <w-desu@i.softbank.jp>
2016-11-29 15:21:18 +0900

    TLを取得できるようにした

--- app.js
+++ app.js
@@ -2,24 +2,27 @@
 
 // TLの画像を保存するやつ
 
-const config = require('./config.js');
-const Twitter = require('twitter');
-const path = require('path');
-const SAVEDIR = path.join(__dirname, 'pics');
+var config = require('./config.js');
+var Twitter = require('twitter');
+var path = require('path');
+var SAVEDIR = path.join(__dirname, 'pics');
 
-const client = new Twitter({
+var client = new Twitter({
     consumer_key: config.consumer_key,
     consumer_secret: config.consumer_secret,
     access_token_key: config.access_token_key,
     access_token_secret: config.access_token_secret
 });
 
-client.stream('statuses/filter', {track: ''}, (stream) => {
-    stream.on('data', tweet => {
-        console.log('on data user: ${tweet.text}');
+client.stream('user', function(stream) {
+    stream.on('data', function(tweet) {
+        // console.log('on data tweet: ' + tweet.text);
+        console.log(tweet.user.name);
+        console.log('@' + tweet.user.screen_name);
+        console.log(tweet.text);
     });
 
-    stream.on('error', error => {
-        console.log('on error: ${error}');
+    stream.on('error', function(error) {
+        console.log('on error: ' + error);
     });
 });
